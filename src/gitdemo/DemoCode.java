dcc8a276c9d2173b4a73e89534a1ef949901ba77
Anurag Ranjan <anuragranjan330@gmail.com>
2020-09-27 20:00:23 +0530

    first commit

---
+++ src/gitdemo/DemoCode.java
@@ -0,0 +1,13 @@
+package gitdemo;
+
+public class DemoCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("1st line of code");
+
+	}
+
+}
+
